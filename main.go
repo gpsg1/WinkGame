d1ad54eb44cee2625f405200c5e7b544ad27a329
lhjun <lhjun0121@gmail.com>
2024-10-01 13:25:16 +0900

    userSelectMenu code

---
+++ main.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var (
+	GuildID        = flag.String("guild", "", "")
+	BotToken       = flag.String("token", "", "")
+	RemoveCommands = flag.Bool("rmcmd", true, "Remove all commands after shutdowning or not")
+	appID          = ""
+)
+
+var session *discordgo.Session
+
+func init() { flag.Parse() }
+
+func init() {
+	var err error
+	session, err = discordgo.New("Bot " + *BotToken)
+	if err != nil {
+		log.Fatalf("Invalid bot parameters: %v", err)
+	}
+}
+
+// Commands
+var (
+	commands = []*discordgo.ApplicationCommand{
+		{
+			Name:        "select",
+			Description: "Select member",
+		},
+	}
+)
+
+func init() {
+	session.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		switch i.Type {
+		case discordgo.InteractionApplicationCommand:
+			switch i.ApplicationCommandData().Name {
+			case "select":
+				selectUserHandler(s, i)
+			}
+		case discordgo.InteractionMessageComponent:
+			switch i.MessageComponentData().CustomID {
+			case "user_select_menu":
+				handleSelectMenu(s, i)
+			case "start_button":
+				handleStartButton(s, i)
+			}
+		}
+	})
+}
+
+func main() {
+	session.AddHandler(func(s *discordgo.Session, r *discordgo.Ready) {
+		log.Printf("Logged in as: %v#%v", s.State.User.Username, s.State.User.Discriminator)
+	})
+	err := session.Open()
+	if err != nil {
+		log.Fatalf("Cannot open the session: %v", err)
+	}
+
+	log.Println("Adding commands...")
+	registeredCommands := make([]*discordgo.ApplicationCommand, len(commands))
+	for i, v := range commands {
+		cmd, err := session.ApplicationCommandCreate(session.State.User.ID, *GuildID, v)
+		if err != nil {
+			log.Panicf("Cannot create '%v' command: %v", v.Name, err)
+		}
+		registeredCommands[i] = cmd
+	}
+
+	defer session.Close()
+
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt)
+	log.Println("Press Ctrl+C to exit")
+	<-stop
+
+	if *RemoveCommands {
+		log.Println("Removing commands...")
+		for _, v := range registeredCommands {
+			err := session.ApplicationCommandDelete(session.State.User.ID, *GuildID, v.ID)
+			if err != nil {
+				log.Panicf("Cannot delete '%v' command: %v", v.Name, err)
+			}
+		}
+	}
+
+	log.Println("Shutting down.")
+}
---
+++ selectUserHandler.go
@@ -0,0 +1,109 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"github.com/bwmarrin/discordgo"
+)
+
+var selectedUsersMap = make(map[string][]string)
+
+func selectUserHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// 길드 멤버 가져오기
+	members, err := s.GuildMembers(*GuildID, "", 25)
+	if err != nil {
+		log.Println("Error fetching members:", err)
+		return
+	}
+
+	// User 목록으로 SelectMenu 생성
+	var options []discordgo.SelectMenuOption
+	for _, member := range members {
+		// member.User.ID와 member.User.Username을 사용하여 옵션 생성
+		options = append(options, discordgo.SelectMenuOption{
+			Label: member.User.GlobalName,
+			Value: member.User.ID,
+		})
+	}
+	MinValues := 1
+	MaxValues := len(options)
+
+	// SelectMenu와 ActionRow 설정
+	selectMenu := discordgo.SelectMenu{
+		CustomID:    "user_select_menu",
+		Placeholder: "Select a user...",
+		MinValues:   &MinValues,
+		MaxValues:   MaxValues,
+		Options:     options,
+	}
+
+	actionRow := discordgo.ActionsRow{
+		Components: []discordgo.MessageComponent{
+			selectMenu,
+		},
+	}
+	buttonRow := discordgo.ActionsRow{
+		Components: []discordgo.MessageComponent{
+			&discordgo.Button{
+				Label:    "Select",                // 버튼 텍스트
+				Style:    discordgo.PrimaryButton, // 버튼 스타일
+				CustomID: "start_button",          // 버튼 클릭 시 처리할 ID
+			},
+		},
+	}
+
+	// 드롭다운 메뉴와 버튼을 포함한 메시지 전송
+	err = s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Components: []discordgo.MessageComponent{
+				actionRow,
+				buttonRow,
+			},
+		},
+	})
+	if err != nil {
+		log.Println("Error responding to interaction:", err)
+		return
+	}
+
+	selectedUsersMap[i.GuildID] = make([]string, 0)
+}
+
+// Select 버튼이 눌렸을 때 선택된 멤버들을 처리하는 핸들러
+func handleStartButton(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// 선택된 멤버 ID 목록을 가져옴
+	tempSelectedMembers := selectedUsersMap[i.GuildID]
+	if len(tempSelectedMembers) == 0 {
+		log.Println("No members selected.")
+		return
+	}
+
+	// 선택된 멤버 ID를 로그에 출력
+	log.Printf("Selected members : %s", strings.Join(tempSelectedMembers, ", "))
+
+	// Interaction 응답 (선택 결과를 유저에게 표시)
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		Type: discordgo.InteractionResponseChannelMessageWithSource,
+		Data: &discordgo.InteractionResponseData{
+			Content: "Selected members: " + strings.Join(tempSelectedMembers, ", "),
+		},
+	})
+	if err != nil {
+		log.Println("Error responding to interaction:", err)
+	}
+}
+
+func handleSelectMenu(s *discordgo.Session, i *discordgo.InteractionCreate) {
+	// Map 변수
+	selectedUsersMap[i.GuildID] = i.MessageComponentData().Values
+
+	err := s.InteractionRespond(i.Interaction, &discordgo.InteractionResponse{
+		// 상호작용 지연
+		Type: discordgo.InteractionResponseDeferredMessageUpdate,
+	})
+	if err != nil {
+		log.Println("Error responding to select menu interaction:", err)
+	}
+}
