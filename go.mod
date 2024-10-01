5d4ed74ce9c163bce82c1f2e305cf8b418576002
Ash <ashkim2001@gmail.com>
2024-10-01 12:02:06 +0900

    Add selection code

---
+++ go.mod
@@ -0,0 +1,3 @@
+module gpsp1/WinkGame
+
+go 1.22.5
---
+++ select_king.go
@@ -0,0 +1,18 @@
+package select_king
+
+import (
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+func get(user_list []string) (string) {
+
+  r := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	// Generate a random number between 0 and 100
+	randomNumber := r.Intn(len(user_list)) // Intn(n) returns a random integer from 0 to n-1, so 101 gives 0 to 100
+
+	fmt.Println(randomNumber) // Print the random number
+  return user_list[randomNumber]
+}
